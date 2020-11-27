d7774e104a826db636a7a2a6ab8059c7207c916e
wangjiaqi12807 <wangjiaqi12807@autohome.com.cn>
2020-11-27 14:15:07 +0800

    train AISHELL-3

--- TTS/tts/utils/text/symbols.py
+++ TTS/tts/utils/text/symbols.py
@@ -5,43 +5,55 @@ Defines the set of symbols used in text input to the model.
 The default is a set of ASCII characters that works well for English or text that has been run
 through Unidecode. For other data, you can modify _characters. See TRAINING_DATA.md for details.
 '''
-def make_symbols(characters, phonemes, punctuations='!\'(),-.:;? ', pad='_', eos='~', bos='^'):# pylint: disable=redefined-outer-name
-    ''' Function to create symbols and phonemes '''
-    _phonemes_sorted = sorted(list(phonemes))
 
-    # Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
-    _arpabet = ['@' + s for s in _phonemes_sorted]
 
-    # Export all symbols:
-    _symbols = [pad, eos, bos] + list(characters) + _arpabet
-    _phonemes = [pad, eos, bos] + list(_phonemes_sorted) + list(punctuations)
+def make_symbols(characters, phonemes_list, punctuations='!\'(),-.:;? ', pad='_', eos='~',
+				 bos='^'):  # pylint: disable=redefined-outer-name
+	''' Function to create symbols and phonemes '''
+	_phonemes_sorted = sorted(phonemes_list)
 
-    return _symbols, _phonemes
+	# Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
+	_arpabet = ['@' + s for s in _phonemes_sorted]
+
+	# Export all symbols:
+	_symbols = [pad, eos, bos] + list(characters) + _arpabet
+	_phonemes = [pad, eos, bos] + list(_phonemes_sorted) + list(punctuations)  # wjq use this
+
+	return _symbols, _phonemes
+
 
 _pad = '_'
 _eos = '~'
 _bos = '^'
 _characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!\'(),-.:;? '
-_punctuations = '!\'(),-.:;? '
+_punctuations = '.?!,'  # wjq use this
 _phoneme_punctuations = '.!;:,?'
 
 # Phonemes definition
-_vowels = 'iyɨʉɯuɪʏʊeøɘəɵɤoɛœɜɞʌɔæɐaɶɑɒᵻ'
-_non_pulmonic_consonants = 'ʘɓǀɗǃʄǂɠǁʛ'
-_pulmonic_consonants = 'pbtdʈɖcɟkɡqɢʔɴŋɲɳnɱmʙrʀⱱɾɽɸβfvθðszʃʒʂʐçʝxɣχʁħʕhɦɬɮʋɹɻjɰlɭʎʟ'
-_suprasegmentals = 'ˈˌːˑ'
-_other_symbols = 'ʍwɥʜʢʡɕʑɺɧ'
-_diacrilics = 'ɚ˞ɫ'
-_phonemes = _vowels + _non_pulmonic_consonants + _pulmonic_consonants + _suprasegmentals + _other_symbols + _diacrilics
+# _vowels = 'iyɨʉɯuɪʏʊeøɘəɵɤoɛœɜɞʌɔæɐaɶɑɒᵻ'
+# _non_pulmonic_consonants = 'ʘɓǀɗǃʄǂɠǁʛ'
+# _pulmonic_consonants = 'pbtdʈɖcɟkɡqɢʔɴŋɲɳnɱmʙrʀⱱɾɽɸβfvθðszʃʒʂʐçʝxɣχʁħʕhɦɬɮʋɹɻjɰlɭʎʟ'
+# _suprasegmentals = 'ˈˌːˑ'
+# _other_symbols = 'ʍwɥʜʢʡɕʑɺɧ'
+# _diacrilics = 'ɚ˞ɫ'
+# _phonemes = _vowels + _non_pulmonic_consonants + _pulmonic_consonants + _suprasegmentals + _other_symbols + _diacrilics
 
-symbols, phonemes = make_symbols(_characters, _phonemes, _punctuations, _pad, _eos, _bos)
+_phonemes_list = []
+with open("characters.txt") as f:
+	while 1:
+		line = f.readline()
+		if not line:
+			break
+		_phonemes_list.append(line.strip())
+
+_symbols, _phonemes = make_symbols(_characters, _phonemes_list, _punctuations, _pad, _eos, _bos)
 
 # Generate ALIEN language
 # from random import shuffle
 # shuffle(phonemes)
 
 if __name__ == '__main__':
-    print(" > TTS symbols {}".format(len(symbols)))
-    print(symbols)
-    print(" > TTS phonemes {}".format(len(phonemes)))
-    print(phonemes)
+	print(" > TTS symbols {}".format(len(_symbols)))
+	print(_symbols)
+	print(" > TTS phonemes {}".format(len(_phonemes)))
+	print(_phonemes)
