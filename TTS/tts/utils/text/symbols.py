95e05b464472064244f6f9532e5aae972de25d4a
wangjiaqi12807 <wangjiaqi12807@autohome.com.cn>
2021-01-22 21:01:17 +0800

    add -

--- TTS/tts/utils/text/symbols.py
+++ TTS/tts/utils/text/symbols.py
@@ -27,7 +27,7 @@ _pad = '_'
 _eos = '~'
 _bos = '^'
 _characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!\'(),-.:;? '
-_punctuations = '/ '  # wjq use this
+_punctuations = '-/ '  # wjq use this
 _phoneme_punctuations = '.!;:,?'
 
 # Phonemes definition
